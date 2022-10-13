39f506b41e13251572b95ac64a9700a0e011e263
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-13 11:07:53 -0400

    Added function for validating first name

--- script.js
+++ script.js
@@ -16,11 +16,29 @@ form.addEventListener("keydown", (e) => {
 // main function for validating the form
 function validateForm() {
     // selectors for all the elements in the form
-    const firstName = document.querySelector("#first-name").value;
-    const lastName = document.querySelector("#last-name").value;
-    const email = document.querySelector("#email").value;
-    const phoneNumber = document.querySelector("#phoneNumber").value;
-    const password = document.querySelector("#password").value;
-    const confirmPassword = document.querySelector("#confirm-password").value;
-    console.log(firstName);
+    const firstName = document.querySelector("#first-name");
+    const lastName = document.querySelector("#last-name");
+    const email = document.querySelector("#email");
+    const phoneNumber = document.querySelector("#phoneNumber");
+    const password = document.querySelector("#password");
+    const confirmPassword = document.querySelector("#confirm-password");
+    
+    const correctFirstNaame = validateFirstName(firstName);
+}
+
+// function to validate whether the entered first name is correct or not
+function validateFirstName(firstName) {
+    const firstNameMessage = document.querySelector("#first-name-message");
+    const value = firstName.value.trim();
+    if (value === "") {
+        firstName.classList.add("incorrect-input");
+        firstNameMessage.classList.add("incorrect-message");
+        firstNameMessage.textContent = "*Name cannot be blank";
+    }
+    else {
+        firstName.classList.add("correct-input");
+        firstNameMessage.classList.add("correct-message");
+        firstNameMessage.textContent = "*Valid name";
+        return true;
+    }
 }
